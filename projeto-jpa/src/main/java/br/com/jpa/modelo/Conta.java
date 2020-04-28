6ce9c1d400f64ae224a5174d43f93aff97f98bd3
Kayton-Jr <klaytonjunior@hotmail.com>
2020-04-28 20:25:43 -0300

    Finalizando os estados sobre jpa

--- projeto-jpa/src/main/java/br/com/jpa/modelo/Conta.java
+++ projeto-jpa/src/main/java/br/com/jpa/modelo/Conta.java
@@ -1,16 +1,20 @@
 package br.com.jpa.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Conta {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Integer agencia;
 	private Integer numero;
 	private String titular;
+	private Double saldo;
 	
 	
 	public Long getId() {
@@ -37,5 +41,13 @@ public class Conta {
 	public void setTitular(String titular) {
 		this.titular = titular;
 	}
+	public Double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
 		
 }
